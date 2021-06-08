c0d5d842c36a5e89cf8657aada43a36c5b7ad67f
An Jihyun <hwiyu25@gmail.com>
2021-06-08 10:52:50 +0900

    OTV_웹툰 상세 페이지 라우트 연결

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect, url_for
 from pymongo import MongoClient
 
 app = Flask(__name__)
 
 client = MongoClient('localhost', 27017)
-db = client.dbsparta
+db = client.team18_OTV
 
 @app.route('/')
 def home():
@@ -17,6 +17,14 @@ def login():
 @app.route('/join')
 def join():
     return render_template('join.html')
+
+@app.route('/webtoons/<id>')
+def webtoon(id):
+    my_webtoon = db.team18_OTV.find_one({'id': int(id)})
+    if my_webtoon is not None:
+        return render_template('detail.html', webtoon=my_webtoon)
+    else:
+        return redirect(url_for('home'))
 
 if __name__ == '__main__':
     app.run('0.0.0.0', port=5000, debug=True)
